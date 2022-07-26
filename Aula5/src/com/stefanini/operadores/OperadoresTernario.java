fe1a6b59f1d69f091944f99dd397eeb40874f6c9
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-26 10:27:08 -0300

    Commit aula5

---
+++ Aula5/src/com/stefanini/operadores/OperadoresTernario.java
@@ -0,0 +1,12 @@
+package com.stefanini.operadores;
+
+public class OperadoresTernario {
+
+	public static void main(String[] args) {
+
+		int nota = 5;
+
+		System.out.println(nota >= 7 ? "Aprovado" : "Reprovado");
+	}
+
+}
