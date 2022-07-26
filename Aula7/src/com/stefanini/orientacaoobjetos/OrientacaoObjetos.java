f066b2ef8cc92befd645a4f3771f6a5eea3fbec1
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-26 13:00:46 -0300

    Commit aula 7 e aula 8

---
+++ Aula7/src/com/stefanini/orientacaoobjetos/OrientacaoObjetos.java
@@ -0,0 +1,11 @@
+package com.stefanini.orientacaoobjetos;
+
+public class OrientacaoObjetos {
+
+	public static void main(String[] args) {
+
+		System.out.println("Vídeo aula assistida!!!");
+
+	}
+
+}
---
+++ Aula8/src/com/stefanini/orientacaoaobjetos/Mercedes.java
@@ -0,0 +1,13 @@
+package com.stefanini.orientacaoaobjetos;
+
+public class Mercedes extends Carro {
+
+	public Mercedes() {
+		this.setMarca("Mercedes");
+	}
+
+	@Override
+	public String acelerar() {
+		return "A Mercedes tem uma aceleração diferente para cada modelo";
+	}
+}
