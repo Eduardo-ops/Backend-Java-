e05fdbf770873f3014501a996f88dfcd8ca1283f
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-28 09:37:14 -0300

    Commit aula13

---
+++ Aula13/src/com/stefanini/estruturasdecontrole/While.java
@@ -0,0 +1,16 @@
+package com.stefanini.estruturasdecontrole;
+
+public class While {
+
+	public static void main(String[] args) {
+
+		int count = 101;
+
+		while (count < 100) {
+			System.out.println("count");
+			//count++;
+		}
+
+	}
+
+}
---
+++ Aula13/src/com/stefanini/estruturasdecontrole/doWhile.java
@@ -0,0 +1,15 @@
+package com.stefanini.estruturasdecontrole;
+
+public class doWhile {
+
+	public static void main(String[] args) {
+
+		int count = 101;
+
+		do {
+			System.out.println("count");
+			count++;
+		} while (count < 100);
+	}
+
+}
