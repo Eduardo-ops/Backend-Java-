323b6d6a89d0b990389631a85f9758a0bec350af
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-28 10:47:29 -0300

    Commit aula15

---
+++ Aula15/src/com/stefanini/cursojavabasico/orientacaoaobjetos/Principal.java
@@ -0,0 +1,64 @@
+package com.stefanini.cursojavabasico.orientacaoaobjetos;
+
+public class Principal {
+	public static void main(String[] args) {
+
+		Carro bmw = new Carro();
+		bmw.setMarca("BMW");
+		bmw.setModelo("M3");
+		bmw.setQuilometrosPorLitro(7.5);
+		bmw.setTaxaAceleracao(50);
+		
+//		BMW bmwTres = null;
+//		
+//		bmwTres.acelerar();
+
+		try {
+			bmw.setVelocidadeMaxima(10);
+		} catch (NegocioException e) {
+			System.out.println(e);
+		} finally{
+			System.out.println("Passou aqui");
+		}
+
+		Carro mercedes = new Carro("Mercedes", "c180");
+
+		BMW bmwDois = new BMW();
+		Tesla tesla = new Tesla();
+		tesla.isDirigeSozinho();
+
+		Carro teslaDois = new Tesla();
+		((Tesla) teslaDois).setDirigeSozinho(true);
+		System.out.println(((Tesla) teslaDois).isDirigeSozinho());
+
+		Carro[] carros = new Carro[] { bmw, bmwDois, tesla, teslaDois };
+
+		for (Carro carro : carros) {
+			if (carro instanceof Tesla) {
+				System.out.println(((Tesla) carro).isDirigeSozinho());
+			}
+		}
+
+		bmwDois.calcularTaxaAceleracao(100.50, 10);
+		teslaDois.calcularTaxaAceleracao(100.50, 10);
+
+		System.out.println(bmwDois.calcularTaxaAceleracao(100.5, 10.5));
+
+		System.out.println(mercedes.getMarca());
+		System.out.println(mercedes.getModelo());
+
+		System.out.println(bmw.getMarca());
+		System.out.println(bmw.getModelo());
+		System.out.println(bmw.getQuilometrosPorLitro());
+		System.out.println(bmw.getTaxaAceleracao());
+		System.out.println(bmw.getVelocidadeMaxima());
+
+		bmw.acelerar();
+		bmw.frear();
+
+		System.out.println(bmw.calcularTaxaAceleracao(bmw.getVelocidadeMaxima(), 0, 10, 0));
+		System.out.println(bmw.calcularTaxaAceleracao(bmw.getVelocidadeMaxima(), 5));
+		System.out.println(bmw.calcularTaxaAceleracao(100, 5));
+	}
+
+}
---
+++ Aula15/src/com/stefanini/cursojavabasico/orientacaoaobjetos/Tesla.java
@@ -0,0 +1,25 @@
+package com.stefanini.cursojavabasico.orientacaoaobjetos;
+
+public class Tesla extends Carro {
+
+	private boolean dirigeSozinho;
+	
+	public Tesla() {
+		super();
+	}
+	
+	@Override
+	public double calcularTaxaAceleracao(double velocidadeFinal, double tempoFinal) {
+		System.out.println("Tesla Sobreescrita");
+		return velocidadeFinal * tempoFinal;
+	}
+
+	public boolean isDirigeSozinho() {
+		return dirigeSozinho;
+	}
+
+	public void setDirigeSozinho(boolean dirigeSozinho) {
+		this.dirigeSozinho = dirigeSozinho;
+	}
+
+}
